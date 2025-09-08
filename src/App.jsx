54ac4e55ab6fafce879e3cac677a23b427e200dc
Adithyan <167198897+VbAdithyan@users.noreply.github.com>
2025-09-08 12:23:03 +0530

    a new paragraph is added

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>This is branch 1</h1>
+      <p>Hai</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
